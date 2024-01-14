75640128c9027d3fd621b330425d78de4a3990b3
Alex Avance <aravance@gmail.com>
2024-01-14 15:26:03 -0600

    Enable treesitter movement and selection

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -9,6 +9,60 @@ require'nvim-treesitter.configs'.setup {
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
+  incremental_selection = {
+      enable = true,
+      keymaps = {
+          init_selection = '<c-space>',
+          node_incremental = '<c-space>',
+          scope_incremental = '<c-s>',
+          node_decremental = '<c-backspace>',
+      },
+  },
+
+  textobjects = {
+      select = {
+          enable = true,
+          lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.outer',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.outer',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.outer',
+          },
+      },
+      move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+              [']m'] = '@function.outer',
+              [']]'] = '@class.outer',
+          },
+          goto_next_end = {
+              [']M'] = '@function.outer',
+              [']['] = '@class.outer',
+          },
+          goto_previous_start = {
+              ['[m'] = '@function.outer',
+              ['[]'] = '@class.outer',
+          },
+          goto_previous_end = {
+              ['[M'] = '@function.outer',
+              ['[['] = '@class.outer',
+          },
+      },
+      swap = {
+          enable = true,
+          swap_next = {
+              ['<leader>a'] = '@parameter.inner',
+          },
+          swap_previous = {
+              ['<leader>A'] = '@parameter.inner',
+          },
+      },
+  },
+
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
