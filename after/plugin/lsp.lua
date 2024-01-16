70832f6ed039d8023cf92355d267c0154d578fd1
Alex Avance <aavance@amazon.com>
2024-01-16 09:08:25 -0600

    Retab

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,51 +3,51 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-    'tsserver',
-    'eslint',
-    'lua_ls',
-    'kotlin_language_server',
-    'gopls',
-    'templ',
+  'tsserver',
+  'eslint',
+  'lua_ls',
+  'kotlin_language_server',
+  'html',
+  'htmx',
 })
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-Space>'] = cmp.mapping.complete(),
 })
 
 lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
+  mapping = cmp_mappings
 })
 
 lsp.on_attach(function(_, bufnr)
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = 'LSP: ' .. desc
-        end
-        vim.keymap.set('n', keys, func, { buffer = bufnr, remap = false, desc = desc })
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
     end
+    vim.keymap.set('n', keys, func, { buffer = bufnr, remap = false, desc = desc })
+  end
 
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { buffer = bufnr, remap = false, desc = 'Signature [H]elp' })
-    nmap("<C-h>", vim.lsp.buf.signature_help, 'Signature [H]elp')
-    nmap("gd", vim.lsp.buf.definition, '[G]oto [D]efinition')
-    nmap("gI", vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-    nmap("gr", vim.lsp.buf.references, '[G]oto [R]eferences')
-    nmap("K", vim.lsp.buf.hover, 'Hover Documentation')
-    nmap("<leader>ca", vim.lsp.buf.code_action, '[C]ode [A]ction')
-    nmap("<leader>rn", vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap("<leader>vd", vim.diagnostic.open_float, '[V]im [D]iagnostics')
-    nmap("]d", vim.diagnostic.goto_next, 'Next [D]iagnostic')
-    nmap("[d", vim.diagnostic.goto_prev, 'Previous [D]iagnostic')
+  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { buffer = bufnr, remap = false, desc = 'Signature [H]elp' })
+  nmap("<C-h>", vim.lsp.buf.signature_help, 'Signature [H]elp')
+  nmap("gd", vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap("gI", vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  nmap("gr", vim.lsp.buf.references, '[G]oto [R]eferences')
+  nmap("K", vim.lsp.buf.hover, 'Hover Documentation')
+  nmap("<leader>ca", vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap("<leader>rn", vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap("<leader>vd", vim.diagnostic.open_float, '[V]im [D]iagnostics')
+  nmap("]d", vim.diagnostic.goto_next, 'Next [D]iagnostic')
+  nmap("[d", vim.diagnostic.goto_prev, 'Previous [D]iagnostic')
 
-    vim.api.nvim_create_autocmd('BufWritePre', {
-        pattern = { '*.tsx', '*.ts', '*.go', '*.templ', '*.lua' },
-        callback = function() vim.lsp.buf.format() end,
-    })
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = { '*.tsx', '*.ts', '*.go', '*.templ', '*.lua' },
+    callback = function() vim.lsp.buf.format() end,
+  })
 end)
 
 local lspconfig = require('lspconfig')
@@ -57,8 +57,8 @@ lsp.configure('htmx', { filetypes = { "html", "templ" } })
 lsp.configure('html', { filetypes = { "html", "templ" } })
 
 lsp.configure('kotlin_language_server', {
-    workspaceFolders = true,
-    root_dir = lspconfig.util.root_pattern("packageInfo", vim.fn.getcwd()),
+  workspaceFolders = true,
+  root_dir = lspconfig.util.root_pattern("packageInfo", vim.fn.getcwd()),
 })
 
 lsp.nvim_workspace()
--- lua/aravance/packer.lua
+++ lua/aravance/packer.lua
@@ -1,81 +1,81 @@
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
-    command = 'source <afile> | PackerSync',
-    group = packer_group,
-    pattern = 'packer.lua',
+  command = 'source <afile> | PackerSync',
+  group = packer_group,
+  pattern = 'packer.lua',
 })
 
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'christoomey/vim-tmux-navigator'
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use {
-        'bluz71/vim-moonfly-colors',
-        as = 'moonfly',
-        config = function()
-            vim.g.moonflyTransparent = true
-            vim.cmd('colorscheme moonfly')
-        end
-    }
-
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            require('nvim-treesitter.install').update({ with_sync = true })()
-        end,
-    }
-
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use('tpope/vim-commentary')
-    use('tpope/vim-surround')
-    use('tpope/vim-repeat')
-    use('tpope/vim-speeddating')
-
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
-
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},         -- Required
-            {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
-            {'saadparwaiz1/cmp_luasnip'}, -- Optional
-            {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},             -- Required
-            {'rafamadriz/friendly-snippets'}, -- Optional
-        }
-    }
-
-    if packer_bootstrap then
-        require('packer').sync()
+  use 'wbthomason/packer.nvim'
+  use 'christoomey/vim-tmux-navigator'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use {
+    'bluz71/vim-moonfly-colors',
+    as = 'moonfly',
+    config = function()
+      vim.g.moonflyTransparent = true
+      vim.cmd('colorscheme moonfly')
     end
+  }
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      require('nvim-treesitter.install').update({ with_sync = true })()
+    end,
+  }
+
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+  use('tpope/vim-commentary')
+  use('tpope/vim-surround')
+  use('tpope/vim-repeat')
+  use('tpope/vim-speeddating')
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },             -- Required
+      { 'williamboman/mason.nvim' },           -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },         -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+      { 'hrsh7th/cmp-buffer' },       -- Optional
+      { 'hrsh7th/cmp-path' },         -- Optional
+      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },             -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
+    }
+  }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
