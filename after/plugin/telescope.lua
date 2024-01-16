184501b903f552c84537c49e67fb01e889d16b62
Alex Avance <aavance@amazon.com>
2024-01-16 09:08:25 -0600

    style: retab

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -11,12 +11,12 @@ vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = '[P]roject [G]rep'
 vim.keymap.set('n', '<leader>pw', builtin.grep_string, { desc = 'Search [P]roject for current [W]ord' })
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[P]roject [F]iles' })
 vim.keymap.set('n', '<leader>/', function()
-    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-    })
+  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
 end, { desc = '[/] Fuzzy search current buffer' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Goto [P]roject git files' })
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end, { desc = '[P]roject Grep [S]earch' })
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
   ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "kotlin", "java" },
 
@@ -10,57 +10,57 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   incremental_selection = {
-      enable = true,
-      keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<c-backspace>',
-      },
+    enable = true,
+    keymaps = {
+      init_selection = '<c-space>',
+      node_incremental = '<c-space>',
+      scope_incremental = '<c-s>',
+      node_decremental = '<c-backspace>',
+    },
   },
 
   textobjects = {
-      select = {
-          enable = true,
-          lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.outer',
-              ['af'] = '@function.outer',
-              ['if'] = '@function.outer',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.outer',
-          },
+    select = {
+      enable = true,
+      lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.outer',
+        ['af'] = '@function.outer',
+        ['if'] = '@function.outer',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.outer',
       },
-      move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-              [']m'] = '@function.outer',
-              [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-              [']M'] = '@function.outer',
-              [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-              ['[m'] = '@function.outer',
-              ['[]'] = '@class.outer',
-          },
-          goto_previous_end = {
-              ['[M'] = '@function.outer',
-              ['[['] = '@class.outer',
-          },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
       },
-      swap = {
-          enable = true,
-          swap_next = {
-              ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-              ['<leader>A'] = '@parameter.inner',
-          },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
       },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
   },
 
   highlight = {
