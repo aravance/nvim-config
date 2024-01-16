c584e626f2629fad4258abb46e07eef72dc7946d
Alex Avance <aavance@amazon.com>
2024-01-16 15:47:31 -0600

    Add remap to fix <c-backspace>

--- lua/aravance/remap.lua
+++ lua/aravance/remap.lua
@@ -1,6 +1,22 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- alacritty + tmux can be difficult, so we remap alacritty to send ^W on ^Backspace
+--[=[
+cat >>~/.config/alacritty/alacritty.toml <<EOF
+[[keyboard.bindings]]
+# tmux doesn't support ctrl-backspace -- map it to ctrl-w
+key = "Back"
+mods = "Control"
+chars = "\u0017"
+EOF
+--]=]
+-- then we remap <c-w> here
+vim.api.nvim_set_keymap("n", "<c-w>", "<c-backspace>", {})
+vim.api.nvim_set_keymap("c", "<c-w>", "<c-backspace>", {})
+vim.api.nvim_set_keymap("i", "<c-w>", "<c-backspace>", {})
+vim.api.nvim_set_keymap("v", "<c-w>", "<c-backspace>", {})
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
