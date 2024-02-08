6791eb2e09e78eb5b70884933c5f1e4d0830b798
Alex Avance <aavance@amazon.com>
2024-02-08 12:23:14 -0600

    Add nvim-genghis

--- lua/aravance/packer.lua
+++ lua/aravance/packer.lua
@@ -81,6 +81,10 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
+  use {
+    'chrisgrieser/nvim-genghis',
+    requires = 'stevearc/dressing.nvim',
+  }
 
   use {
     'VonHeikemen/lsp-zero.nvim',
