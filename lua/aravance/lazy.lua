4e88cbcd84a8d8c8368410c4749a997ab49ecd9a
Alex Avance <aavance@amazon.com>
2024-02-14 08:52:09 -0600

    fix: make moonfly eager load

--- lua/aravance/lazy.lua
+++ lua/aravance/lazy.lua
@@ -38,9 +38,11 @@ return require('lazy').setup({
   {
     'bluz71/vim-moonfly-colors',
     name = 'moonfly',
+    lazy = false,
+    priority = 1000,
     config = function()
       vim.g.moonflyTransparent = true
-      vim.cmd('colorscheme moonfly')
+      vim.cmd [[colorscheme moonfly]]
     end
   },
 
