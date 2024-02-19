204813847129c6d786ee7a302feef37abe01f68a
Alex Avance <aravance@gmail.com>
2024-02-19 12:46:38 -0600

    Remove lsp-zero

--- lua/aravance/plugins/formatter.lua
+++ lua/aravance/plugins/formatter.lua
@@ -1,24 +1,32 @@
 return {
-     'mhartington/formatter.nvim',
-config = function()
-require('formatter').setup {
-  filetype = {
-    go = {
-      require('formatter.filetypes.go').goimports,
-    },
-    kotlin = {
-      require('formatter.filetypes.kotlin').ktlint,
-    },
-    ['*'] = {
-      require('formatter.filetypes.any').remove_trailing_whitespace
-    },
-  }
-}
+  'mhartington/formatter.nvim',
+  config = function()
+    require('formatter').setup {
+      filetype = {
+        go = {
+          require('formatter.filetypes.go').goimports,
+        },
+        kotlin = {
+          require('formatter.filetypes.kotlin').ktlint,
+        },
+        lua = {
+          vim.lsp.buf.format,
+        },
+        templ = {
+          function()
+            vim.cmd [[silent %!templ fmt]]
+          end,
+        },
+        ['*'] = {
+          require('formatter.filetypes.any').remove_trailing_whitespace
+        },
+      }
+    }
 
-vim.api.nvim_create_augroup('__formatter__', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  group = '__formatter__',
-  command = ':FormatWrite',
-})
-end,
+    vim.api.nvim_create_augroup('__formatter__', { clear = true })
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      group = '__formatter__',
+      command = ':FormatWrite',
+    })
+  end,
 }
