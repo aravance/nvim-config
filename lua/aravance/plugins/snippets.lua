f0ad94c5d7f1bd1b4ba900f899bf8994e8109056
Alex Avance <aravance@gmail.com>
2024-02-20 13:22:22 -0600

    feat: add snippets.lua

---
+++ lua/aravance/plugins/snippets.lua
@@ -0,0 +1,13 @@
+return {
+  "L3MON4D3/LuaSnip",
+  dependencies = { "rafamadriz/friendly-snippets" }, -- Optional
+  keys = {
+    { mode = "i",          "<C-K>", function() require("luasnip")() end,   desc = "Expand snippet" },
+    { mode = { "i", "s" }, "<C-L>", function() require("luasnip")(1) end,  desc = "Next snippet var" },
+    { mode = { "i", "s" }, "<C-J>", function() require("luasnip")(-1) end, desc = "Previous snippet var" },
+  },
+  config = function()
+    -- load rafamadriz/friendly-snippets
+    require("luasnip.loaders.from_vscode").lazy_load()
+  end,
+}
