5db3f8255960ecd95cf43e675937f1cd2bb9c9dc
Alex Avance <aavance@amazon.com>
2024-02-21 11:15:19 -0600

    feat: change harpoon file bindings

--- lua/aravance/plugins/harpoon.lua
+++ lua/aravance/plugins/harpoon.lua
@@ -2,11 +2,12 @@ return {
   "theprimeagen/harpoon",
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
-    { "<leader>m", function() require("harpoon.mark").add_file() end,        desc = "Harpoon [M]ark file" },
     { "<C-g>",     function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon Menu" },
-    { "<C-j>",     function() require("harpoon.ui").nav_file(1) end,         desc = "Harpoon file 1" },
-    { "<C-k>",     function() require("harpoon.ui").nav_file(2) end,         desc = "Harpoon file 2" },
-    { "<C-l>",     function() require("harpoon.ui").nav_file(3) end,         desc = "Harpoon file 3" },
-    { "<C-'>",     function() require("harpoon.ui").nav_file(4) end,         desc = "Harpoon file 4" },
+    { "<leader>m", function() require("harpoon.mark").add_file() end,        desc = "Harpoon [M]ark file" },
+    { "<leader>1", function() require("harpoon.ui").nav_file(1) end,         desc = "Harpoon file 1" },
+    { "<leader>2", function() require("harpoon.ui").nav_file(2) end,         desc = "Harpoon file 2" },
+    { "<leader>3", function() require("harpoon.ui").nav_file(3) end,         desc = "Harpoon file 3" },
+    { "<leader>4", function() require("harpoon.ui").nav_file(4) end,         desc = "Harpoon file 4" },
+    { "<leader>5", function() require("harpoon.ui").nav_file(5) end,         desc = "Harpoon file 5" },
   },
 }
