194abac9931d1771757bb95c7f7bfad6ac2272dd
Alex Avance <aravance@gmail.com>
2024-02-21 13:51:17 -0600

    fix: remove unnecessary colors function

--- lua/aravance/plugins/colors.lua
+++ lua/aravance/plugins/colors.lua
@@ -1,11 +1,3 @@
-function ColorMyPencils(color)
-  color = color or "moonfly"
-  vim.cmd.colorscheme(color)
-
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
   "bluz71/vim-moonfly-colors",
   name = "moonfly",
@@ -14,6 +6,5 @@ return {
   config = function()
     vim.g.moonflyTransparent = true
     vim.cmd [[colorscheme moonfly]]
-    ColorMyPencils()
   end
 }
