7d6e8b6aa0d210f053a41daacea15c461541a946
Alex Avance <aavance@amazon.com>
2024-02-21 10:06:07 -0600

    feat: migrate to conform for formatting

---
+++ lua/aravance/plugins/conform.lua
@@ -0,0 +1,17 @@
+return {
+  "stevearc/conform.nvim",
+  keys = {
+    { mode = { "n", "x" }, "<leader>f", function() require("conform").format({ lsp_fallback = true }) end, desc = "[F]ormat file" },
+  },
+  opts = {
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+    formatters_by_ft = {
+      kotlin = { "ktlint" },
+      go = { "goimports" },
+      templ = { "templ" },
+    },
+  },
+}
