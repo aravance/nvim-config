099aa2eef8577a333ba6f4cd972f917bc12a6e16
Alex Avance <aravance@gmail.com>
2024-02-21 19:45:06 -0600

    feat: add sort keymap

--- lua/aravance/remap.lua
+++ lua/aravance/remap.lua
@@ -48,3 +48,5 @@ keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
 keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make executable" })
 
 keymap("c", "w!!", "execute 'silent! write !sudo tee % >/dev/null' <bar> edit!")
+
+keymap("v", "gs", "<cmd>'<,'>sort<CR>", "Sort lines")
