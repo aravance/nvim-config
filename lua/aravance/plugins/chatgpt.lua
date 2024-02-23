9975b216b505a9b32c2bda407d64e2fb4cde5ec7
Alex Avance <aravance@gmail.com>
2024-02-23 16:54:37 -0600

    feat: add chatgpt plugin

---
+++ lua/aravance/plugins/chatgpt.lua
@@ -0,0 +1,13 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    api_key_cmd = "bw get notes openai-api-key",
+  },
+}
