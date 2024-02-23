a247024677e720ecdb457e5f57827eff1650aa51
Alex Avance <aavance@amazon.com>
2024-02-22 21:37:44 -0600

    feat: change mini.surround mappings to more closely mirror tpope/vim-surround

--- lua/aravance/plugins/init.lua
+++ lua/aravance/plugins/init.lua
@@ -3,5 +3,18 @@ return {
   { "echasnovski/mini.ai",        opts = {} },
   { "echasnovski/mini.bracketed", opts = {} },
   { "echasnovski/mini.comment",   opts = {} },
-  { "echasnovski/mini.surround",  opts = {} },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "ys",
+        delete = "ds",
+        find = "",
+        find_left = "",
+        highlight = "",
+        replace = "cs",
+        update_n_lines = "",
+      },
+    },
+  },
 }
