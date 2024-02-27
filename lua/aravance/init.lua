dac344382c3e5f9ad1756472d714e6477a7028ce
Alex Avance <aavance@amazon.com>
2024-02-27 12:16:03 -0600

    Add lsp quickfix commands alongside telescope calls

--- lua/aravance/init.lua
+++ lua/aravance/init.lua
@@ -16,19 +16,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
       vim.keymap.set(mode, keys, func, { buffer = e.buf, remap = false, desc = desc })
     end
 
-    local telescope = require("telescope.builtin")
-
     map("n", "<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
     map("n", "<leader>vd", vim.diagnostic.open_float, "[V]im [D]iagnostics")
     map({ "n", "x" }, "<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
     map({ "i", "n" }, "<C-h>", vim.lsp.buf.signature_help, "Signature [H]elp")
     map("n", "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-    map("n", "gd", telescope.lsp_definitions, "[G]oto [D]efinition")
-    map("n", "gy", telescope.lsp_type_definitions, "[G]oto T[y]pe Definition")
-    map("n", "gi", telescope.lsp_implementations, "[G]oto [I]mplementation")
-    map("n", "gr", telescope.lsp_references, "[G]oto [R]eferences")
-    map("n", "<leader>ds", telescope.lsp_document_symbols, "[D]ocument [S]ymbols")
-    map("n", "<leader>ws", telescope.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+    map("n", "gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+    map("n", "gy", vim.lsp.buf.type_definition, "[G]oto T[y]pe Definition")
+    map("n", "gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+    map("n", "gr", vim.lsp.buf.references, "[G]oto [R]eferences")
     map("n", "K", vim.lsp.buf.hover, "Hover Documentation")
   end,
 })
