2a9fb4c0a4899552361a52b8e7e7badeb1c338bd
Alex Avance <aavance@amazon.com>
2024-02-27 12:16:03 -0600

    feat: add lsp quickfix commands alongside telescope calls

--- lua/aravance/plugins/telescope.lua
+++ lua/aravance/plugins/telescope.lua
@@ -17,14 +17,20 @@ return {
     end
 
     return {
-      { "<leader>ht", builtin.help_tags,     desc = "[H]elp [t]ags" },
-      { "<leader>hk", builtin.keymaps,       desc = "[H]elp [k]eymaps" },
-      { "<leader>pd", builtin.diagnostics,   desc = "[P]roject [d]iagnostics" },
-      { "<leader>pf", builtin.find_files,    desc = "[P]roject [f]iles" },
-      { "<leader>ph", find_hidden_files,     desc = "[P]roject files (+[h]idden)" },
-      { "<leader>/",  search_current_buffer, desc = "Fuzzy search current buffer" },
-      { "<C-p>",      builtin.git_files,     desc = "Goto [p]roject git files" },
-      { "<leader>pg", builtin.live_grep,     desc = "[P]roject live [g]rep" },
+      { "<leader>gd", builtin.lsp_definitions,               desc = "[G]oto [D]efinition" },
+      { "<leader>gy", builtin.lsp_type_definitions,          desc = "[G]oto T[y]pe Definition" },
+      { "<leader>gi", builtin.lsp_implementations,           desc = "[G]oto [I]mplementation" },
+      { "<leader>gr", builtin.lsp_references,                desc = "[G]oto [R]eferences" },
+      { "<leader>ds", builtin.lsp_document_symbols,          desc = "[D]ocument [S]ymbols" },
+      { "<leader>ws", builtin.lsp_dynamic_workspace_symbols, desc = "[W]orkspace [S]ymbols" },
+      { "<leader>ht", builtin.help_tags,                     desc = "[H]elp [t]ags" },
+      { "<leader>hk", builtin.keymaps,                       desc = "[H]elp [k]eymaps" },
+      { "<leader>pd", builtin.diagnostics,                   desc = "[P]roject [d]iagnostics" },
+      { "<leader>pf", builtin.find_files,                    desc = "[P]roject [f]iles" },
+      { "<leader>ph", find_hidden_files,                     desc = "[P]roject files (+[h]idden)" },
+      { "<leader>/",  search_current_buffer,                 desc = "Fuzzy search current buffer" },
+      { "<C-p>",      builtin.git_files,                     desc = "Goto [p]roject git files" },
+      { "<leader>pg", builtin.live_grep,                     desc = "[P]roject live [g]rep" },
       {
         "<leader>pw",
         function()
